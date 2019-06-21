38625fc0bc46a3731773f89ed8407c2f8f22d158
Alok Singh <aloksingh@Aloks-MacBook-Pro.local>
2019-06-21 11:54:43 +0530

    Added Basic Spring Security and Jwt Interface

--- ppmtool/src/main/java/com/alok14741/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
+++ ppmtool/src/main/java/com/alok14741/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
@@ -24,4 +24,11 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
         ProjectNotFoundExceptionResponse exceptionResponse = new ProjectNotFoundExceptionResponse(ex.getMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
+
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleUsernameAlreadyExistsException(UserNameAlreadyExistsException ex, WebRequest request) {
+        UserNameAlreadyExistsResponse exceptionResponse = new UserNameAlreadyExistsResponse(ex.getMessage());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
 }
---
+++ ppmtool/src/main/java/com/alok14741/ppmtool/web/UserController.java
@@ -0,0 +1,40 @@
+package com.alok14741.ppmtool.web;
+
+import com.alok14741.ppmtool.domain.User;
+import com.alok14741.ppmtool.services.MapValidationErrorService;
+import com.alok14741.ppmtool.services.UserService;
+import com.alok14741.ppmtool.validator.UserValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserValidator userValidator;
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
+        userValidator.validate(user, result);
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+        if (errorMap != null) return errorMap;
+
+        User newUser = userService.saveUser(user);
+
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+}
